729f7647c60c330e2932b7a45e8f21b658efa179
ed23x <140010230+ed23x@users.noreply.github.com>
2025-01-18 00:45:56 +0100

    a

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     target: "esnext",
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          slidev: [
-            "@slidev/client",
-            "@slidev/theme-default",
-            "@slidev/theme-seriph",
-          ],
-        },
-      },
-    },
+    assetsDir: "assets",
   },
 });
